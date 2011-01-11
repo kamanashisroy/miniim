19d387dd5efa49d4dbd249e821ac8255e14ec280
Kazi Maraz <root@localhost.localdomain>
2011-01-12 03:13:33 +0600

    adding qt support

---
+++ platform/qt/src/ui/core/xultb_window.cpp
@@ -0,0 +1,37 @@
+/*
+ * This file part of MiniIM.
+ *
+ * Copyright (C) 2007  Kamanashis Roy
+ *
+ * MiniIM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MiniIM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MiniIM.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+#include <QtGui>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "ui/core/xultb_window.h"
+
+
+struct xultb_window*create_window() {
+
+	QMainWindow qwindow = QMainWindow::New(0,0);
+
+}
+
+#ifdef __cplusplus
+}
+#endif
