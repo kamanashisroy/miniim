aafbdb64ff86261edeb10e30ccb3dc8dafc14646
Kazi Maraz <root@localhost.localdomain>
2011-01-14 02:34:33 +0600

    restructured directory tree

---
+++ core/src/test/test.c
@@ -0,0 +1,12 @@
+
+#include "core/xultb_decorator.h"
+#include "core/xultb_exttypes.h"
+
+
+
+int main(void) {
+
+	// TODO write test code
+	return 0;
+}
+
---
+++ markup/src/test/test.c
@@ -0,0 +1,10 @@
+
+#include "ui/markup/extended/xultb_markup_list.h"
+
+
+int main(void) {
+
+	test_window();
+	return 0;
+}
+
